8475a8b9d89780b9fa7fa7284839c818956a755b
Grantseltzer <grantseltzer@gmail.com>
2016-06-08 15:40:47 -0400

    ParseAction function, fixed parseFlagOpt function

--- parse.go
+++ parse.go
@@ -1,19 +1,53 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"os"
+	"strings"
 
-func parseFlagOpt(action string, arguments string) {
+	"github.com/docker/engine-api/types"
+)
 
-	var list []string
+func parseFlagOpt(action string, arguments string, config *types.Seccomp) {
+
+	var syscalls []string
 	if strings.Contains(arguments, ",") {
-		list = strings.Split(arguments, ",")
-	} else if strings.Contains(arguments, "/") {
-		list = strings.Split(arguments, "/")
+		syscalls = strings.Split(arguments, ",")
+	}
+	if strings.Contains(arguments, "/") {
+		syscalls = strings.Split(arguments, "/")
 	}
 
-	updateAction(action, list)
+	correctedAction := parseAction(action)
+
+	for _, syscall := range syscalls {
+		for _, syscallStruct := range config.Syscalls {
+			if syscallStruct.Name == syscall {
+				syscallStruct.Action = correctedAction
+			}
+		}
+	}
+
+	// Return some type of error?
 }
 
-func updateAction(action string, list []string) {
+// Take passed action, return the SCMP_ACT_<ACTION> version of it
+func parseAction(action string) types.Action {
+	switch action {
+	case "kill":
+		return types.ActKill
+	case "trap":
+		return types.ActTrap
+	case "errno":
+		return types.ActErrno
+	case "trace":
+		return types.ActTrace
+	case "allow":
+		return types.ActAllow
+	default:
+		fmt.Println("Unrecognized action", action)
+		os.Exit(-3)
+		return types.ActKill
+	}
 
 }
