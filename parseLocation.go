11f46b9e227035d0a3542baf03c1eb40dbc245af
Grantseltzer <grantseltzer@gmail.com>
2016-06-15 16:36:58 -0400

    Added cwd function for better default location

--- parseLocation.go
+++ parseLocation.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"os"
 	"os/user"
 	"strings"
 	"time"
 )
 
+func defaultFullPath() string {
+	return (parseLocation(pwd(), parseTime()))
+}
+
 func parseLocation(location, name string) string {
 	return strings.TrimSuffix(location, "/") + "/" + name + ".json"
 }
 
-//returns current time and date as a string without any whitespace
 func parseTime() string {
 	return strings.Replace(time.Now().String(), " ", "", -1)
 }
@@ -19,4 +23,10 @@ func userHomeDir() string {
 	usr, err := user.Current()
 	fatalErrorCheck(err, "Could not obtain users home directory. Try setting a custom output location with -location")
 	return usr.HomeDir
+}
+
+func pwd() string {
+	pwd, err := os.Getwd()
+	fatalErrorCheck(err, "Couldn't get current working directory")
+	return pwd
 }
