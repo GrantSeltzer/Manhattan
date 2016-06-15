644de9bda8502a533ab46b667797adfbb24d1446
Grantseltzer <grantseltzer@gmail.com>
2016-06-15 16:35:07 -0400

    Needed conditional for the passed bool

--- arguments.go
+++ arguments.go
@@ -9,20 +9,22 @@ import (
 )
 
 func parseArguments(needed bool, delimArgs []string, syscallStruct *types.Syscall) {
-	syscallIndex, _ := strconv.ParseUint(delimArgs[1], 10, 0)
-	syscallValue, _ := strconv.ParseUint(delimArgs[2], 10, 64)
-	syscallValueTwo, _ := strconv.ParseUint(delimArgs[3], 10, 64)
-	syscallOp := parseOperator(delimArgs[4])
+	if needed {
+		syscallIndex, _ := strconv.ParseUint(delimArgs[1], 10, 0)
+		syscallValue, _ := strconv.ParseUint(delimArgs[2], 10, 64)
+		syscallValueTwo, _ := strconv.ParseUint(delimArgs[3], 10, 64)
+		syscallOp := parseOperator(delimArgs[4])
 
-	ArgStruct := types.Arg{
-		Index:    uint(syscallIndex),
-		Value:    syscallValue,
-		ValueTwo: syscallValueTwo,
-		Op:       syscallOp,
+		ArgStruct := types.Arg{
+			Index:    uint(syscallIndex),
+			Value:    syscallValue,
+			ValueTwo: syscallValueTwo,
+			Op:       syscallOp,
+		}
+		var ArgSlice []*types.Arg
+		ArgSlice = append(ArgSlice, &ArgStruct)
+		syscallStruct.Args = ArgSlice
 	}
-	var ArgSlice []*types.Arg
-	ArgSlice = append(ArgSlice, &ArgStruct)
-	syscallStruct.Args = ArgSlice
 }
 
 func parseOperator(operator string) types.Operator {
