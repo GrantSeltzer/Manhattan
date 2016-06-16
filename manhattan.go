08d9fdb725286e24170966cde7b050d2cb458d91
Grantseltzer <grantseltzer@gmail.com>
2016-06-16 12:32:57 -0400

    Revamped to use cli library, now supports double dashes

--- manhattan.go
+++ manhattan.go
@@ -2,37 +2,106 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
 	"os"
 
 	"github.com/docker/engine-api/types"
+	"github.com/urfave/cli"
 )
 
 const defaultSeccompProfile = "/etc/sysconfig/manhattan.json"
 
 func main() {
 
-	input := flag.String("input", defaultSeccompProfile, "Specify location of base configuration file")
-	kill := flag.String("kill", "", "Respond to system call with KILL")
-	trap := flag.String("trap", "", "Respond to system call with TRAP")
-	errno := flag.String("errno", "", "Respond to system call with ERRNO")
-	trace := flag.String("trace", "", "Respond to system call with TRACE")
-	allow := flag.String("allow", "", "Respond to system call with ALLOW")
-	remove := flag.String("remove", "", "Remove a syscall ")
-	defaultAction := flag.String("default", "errno", "Set the default action")
-	arch := flag.String("arch", "amd64,x86,x32", "Set supported architectures")
-	name := flag.String("name", defaultFullPath(), "Set name of output file")
-	// version := flag.String("version", "nvm", "Print version")
+	var (
+		input         string
+		kill          string
+		trap          string
+		errno         string
+		trace         string
+		allow         string
+		remove        string
+		defaultAction string
+		arch          string
+		name          string
+	)
 
-	flag.Parse()
-	// checkVersion(*version)
+	app := cli.NewApp()
+	app.Name = "manhattan"
+	app.Usage = "Create Docker compliant seccomp json configurations"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "input, i",
+			Value:       defaultSeccompProfile,
+			Usage:       "Specify location of base configuration file",
+			Destination: &input,
+		},
+		cli.StringFlag{
+			Name:        "kill, k",
+			Value:       "",
+			Usage:       "Respond to system call with KILL",
+			Destination: &kill,
+		},
+		cli.StringFlag{
+			Name:        "trap, tp",
+			Value:       "",
+			Usage:       "Respond to system call with TRAP",
+			Destination: &trap,
+		},
+		cli.StringFlag{
+			Name:        "errno, e",
+			Value:       "",
+			Usage:       "Respond to system call with ERRNO",
+			Destination: &errno,
+		},
+		cli.StringFlag{
+			Name:        "trace, tc",
+			Value:       "",
+			Usage:       "Respond to system call with TRACE",
+			Destination: &trace,
+		},
+		cli.StringFlag{
+			Name:        "allow, al",
+			Value:       "",
+			Usage:       "Respond to system call with ALLOW",
+			Destination: &allow,
+		},
+		cli.StringFlag{
+			Name:        "remove, r",
+			Value:       "",
+			Usage:       "Remove a syscall",
+			Destination: &remove,
+		},
+		cli.StringFlag{
+			Name:        "default, d",
+			Value:       "errno",
+			Usage:       "Set the default action for syscalls not specified",
+			Destination: &defaultAction,
+		},
+		cli.StringFlag{
+			Name:        "arch, ar",
+			Value:       "amd64,x86,x32",
+			Usage:       "Set supported architectures",
+			Destination: &arch,
+		},
+		cli.StringFlag{
+			Name:        "name, n",
+			Value:       defaultFullPath(),
+			Usage:       "Set name of output file",
+			Destination: &name,
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		return nil
+	}
+
+	app.Run(os.Args)
 
 	var SeccompProfile types.Seccomp
 
-	configFile, configError := os.Open(*input)
+	configFile, configError := os.Open(input)
 	if configError != nil {
-		fmt.Println("[*] Could not open seccomp profile at", *input)
+		fmt.Println("[*] Could not open seccomp profile at", input)
 		fmt.Println("[*] Creating new Profile")
 	} else {
 		jsonParser := json.NewDecoder(configFile)
@@ -41,19 +110,19 @@ func main() {
 	}
 	defer configFile.Close()
 
-	parseSysCallFlag("kill", *kill, &SeccompProfile)
-	parseSysCallFlag("trap", *trap, &SeccompProfile)
-	parseSysCallFlag("errno", *errno, &SeccompProfile)
-	parseSysCallFlag("trace", *trace, &SeccompProfile)
-	parseSysCallFlag("allow", *allow, &SeccompProfile)
-	parseDefaultAction(*defaultAction, &SeccompProfile)
-	parseArchFlag(*arch, &SeccompProfile)
-	removeAction(*remove, &SeccompProfile)
+	parseSysCallFlag("kill", kill, &SeccompProfile)
+	parseSysCallFlag("trap", trap, &SeccompProfile)
+	parseSysCallFlag("errno", errno, &SeccompProfile)
+	parseSysCallFlag("trace", trace, &SeccompProfile)
+	parseSysCallFlag("allow", allow, &SeccompProfile)
+	parseDefaultAction(defaultAction, &SeccompProfile)
+	parseArchFlag(arch, &SeccompProfile)
+	removeAction(remove, &SeccompProfile)
 
 	b, marshallError := json.MarshalIndent(SeccompProfile, "", "    ")
 	fatalErrorCheck(marshallError, "Error creating Seccomp Profile")
 
-	newConfigFile, newConfigError := os.Create(*name)
+	newConfigFile, newConfigError := os.Create(name)
 	fatalErrorCheck(newConfigError, "Error creating config file")
 	_, writeError := newConfigFile.Write(b)
 	fatalErrorCheck(writeError, "Error writing config to file")
