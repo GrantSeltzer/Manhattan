bcbd82fc46bfa180cbc4c0b2087374d018205171
Grantseltzer <grantseltzer@gmail.com>
2016-06-25 22:15:07 -0400

    Wrote helper functions that will be used for determining actions for new syscalls

--- ociseccompgen/syscall_compare.go
+++ ociseccompgen/syscall_compare.go
@@ -6,22 +6,105 @@ import (
 	types "github.com/opencontainers/runtime-spec/specs-go"
 )
 
-func compareSyscalls(config1, config2 *types.Syscall) bool {
-	totalSimilarities := 0
+/*******************************************************
+This file is a bunch of helper functions for determining
+what action should be taken for new syscalls. See flowchart
+for more details.
+********************************************************/
 
+func hasArguments(config *types.Syscall) bool {
+	nilSyscall := new(types.Syscall)
+	if sameArgs(nilSyscall, config) {
+		return false
+	}
+	return true
+}
+
+func identical(config1, config2 *types.Syscall) bool {
+	if reflect.DeepEqual(config1, config2) {
+		return true
+	}
+	return false
+}
+
+func identicalExceptAction(config1, config2 *types.Syscall) bool {
+	samename := sameName(config1, config2)
+	sameAction := sameAction(config1, config2)
+	sameArgs := sameArgs(config1, config2)
+
+	if samename && !sameAction && sameArgs {
+		return true
+	}
+	return false
+}
+
+func identicalExceptArgs(config1, config2 *types.Syscall) bool {
+	samename := sameName(config1, config2)
+	sameAction := sameAction(config1, config2)
+	sameArgs := sameArgs(config1, config2)
+
+	if samename && sameAction && !sameArgs {
+		return true
+	}
+	return false
+}
+
+func sameName(config1, config2 *types.Syscall) bool {
 	if config1.Name == config2.Name {
-		totalSimilarities++
+		return true
 	}
+	return false
+}
 
+func sameAction(config1, config2 *types.Syscall) bool {
 	if config1.Action == config2.Action {
-		totalSimilarities++
+		return true
 	}
+	return false
+}
 
+func sameArgs(config1, config2 *types.Syscall) bool {
 	if reflect.DeepEqual(config1.Args, config2.Args) {
-		totalSimilarities++
+		return true
 	}
+	return false
+}
 
-	if totalSimilarities == 3 {
+func bothHaveArgs(config1, config2 *types.Syscall) bool {
+	conf1 := hasArguments(config1)
+	conf2 := hasArguments(config2)
+
+	if conf1 && conf2 {
+		return true
+	}
+	return false
+}
+
+func onlyOneHasArgs(config1, config2 *types.Syscall) bool {
+	conf1 := hasArguments(config1)
+	conf2 := hasArguments(config2)
+
+	if (conf1 && !conf2) || (!conf1 && conf2) {
+		return true
+	}
+	return false
+}
+
+func neitherHasArgs(config1, config2 *types.Syscall) bool {
+	conf1 := hasArguments(config1)
+	conf2 := hasArguments(config2)
+
+	if !conf1 && !conf2 {
+		return true
+	}
+	return false
+}
+
+func firstParamOnlyHasArgs(config1, config2 *types.Syscall) bool {
+	conf1 := hasArguments(config1)
+	conf2 := hasArguments(config2)
+
+	if conf1 && !conf2 {
 		return true
 	}
 	return false
